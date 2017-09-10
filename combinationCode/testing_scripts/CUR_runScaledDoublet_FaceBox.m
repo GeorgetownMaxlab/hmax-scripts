fa6fc999f6cc726c18a628481763af46e3ef2683
Levan Bokeria <levan.bokeria@gmail.com>
2017-09-10 13:16:56 +0200

    added some files

---
+++ combinationCode/testing_scripts/CUR_runScaledDoublet_FaceBox.m
@@ -0,0 +1,122 @@
+function CUR_runScaledDoublet_FaceBox(nTPatches,nCPatches,combination_type)
+% run on the other set. 
+
+% This script uses the face-box data. Use this script to evaluate doublets
+% on the testing set of the Jacob's pictures. Use the
+% CUR_runScaledDoublet_wedge30.m to evaluate doublets on the images that
+% Florence's subject's saw.
+
+% clear; clc;
+dbstop if error;
+
+% if (nargin < 4)
+%     CPatchThreshold = 20;
+% end
+if (nargin < 3)
+%     combination_type = 'find_CPatches';
+%     combination_type = fullfile('find_CPatches_thresholding',['CPatchThreshold_' int2str(CPatchThreshold)]);
+    combination_type = 'find_CPatches';
+end
+if (nargin < 1)  
+    nTPatches = 1000;
+    nCPatches = 100;
+end
+
+if ispc
+    home = 'C:\Users\levan\HMAX\annulusExptFixedContrast';
+else
+    home = '/home/levan/HMAX/annulusExptFixedContrast';
+end
+conditionLoad_testing  = fullfile('simulation4','control_v2');
+conditionLoad_training = fullfile('simulation4','training');
+conditionSplit = 'patchSet_3x2';
+perfUsedTraining = 'fbox';
+perfUsedTesting  = 'fbox';
+
+loadLoc_singles        = fullfile(home,conditionLoad_testing,'data',conditionSplit,'lfwSingle50000');
+saveLoc                = fullfile(home,conditionLoad_testing,'data',conditionSplit,'lfwSingle50000','combinations',...
+                                  combination_type,'doublets',...
+                                 [int2str(nTPatches) 'TPatches' int2str(nCPatches) 'CPatches_' ...
+                                 perfUsedTraining '_x_' perfUsedTesting]);
+combMatrixLoc_doublets = fullfile(home,conditionLoad_training,'data',conditionSplit,'lfwSingle50000','combinations',...
+                                  combination_type,'doublets',...
+                                  [int2str(nTPatches) 'TPatches' int2str(nCPatches) 'CPatches']...
+                                  );
+
+if ~exist(saveLoc,'dir')
+    mkdir(saveLoc)
+end
+
+
+runParameterComments = 'none';%input('Any comments about the run?\n'); %#ok<*NASGU>
+
+load(fullfile(combMatrixLoc_doublets,'combMatrix'));
+save(fullfile(saveLoc,'combMatrix_Source'),'combMatrix');
+c2f_testing_singles = load(fullfile(loadLoc_singles,'c2f')); % the variable name says "testing" but its actually c2 file for training set. See above description.
+c2f_testing_singles = c2f_testing_singles.c2f;
+imgHitsFaceBox_testing_singles = load(fullfile(loadLoc_singles,'fixedLocalization','imgHitsFaceBox'));
+imgHitsFaceBox_testing_singles = imgHitsFaceBox_testing_singles.imgHitsFaceBox;
+% imgHitsFaceBox_testing_singles = imgHitsFaceBox_testing_singles.wedgeDegree_30;
+nImgs = size(c2f_testing_singles,2);
+%% Start main script
+nDoublets         = size(combMatrix,1);
+combMatrix_output = zeros(nDoublets,5);
+
+% Reorganize combMatrix_source so that its ordered by the best patches.
+[~,ind] = sort(combMatrix(:,4),'descend');
+combMatrix = combMatrix(ind,:);
+
+for iDoublet = 1:nDoublets
+    pushValue = 0.001;
+
+    if mod(iDoublet,1000) == 0
+        iDoublet
+    end
+    curDoubletInfo = combMatrix(iDoublet,:);
+    
+    % Use the pushValue to make sure two C2 outputs aren't exactly the same
+    if curDoubletInfo(3) < 1
+        pushValue = -pushValue;
+    end
+    
+    newC2_Scaled    = [c2f_testing_singles(curDoubletInfo(1),:);...
+                       c2f_testing_singles(curDoubletInfo(2),:) .* curDoubletInfo(3) + pushValue]; 
+           
+    newImgHitsFaceBox = [imgHitsFaceBox_testing_singles(curDoubletInfo(1),:); ...
+                         imgHitsFaceBox_testing_singles(curDoubletInfo(2),:)];
+    
+    [newC2_min_Scaled(1,:),chosenPatchIdx(1,:)] = min(newC2_Scaled,[],1);
+    
+    newImgHitsFaceBox_Scaled = zeros(1,nImgs); % preallocate
+    for iImg = 1:nImgs
+        newImgHitsFaceBox_Scaled(1,iImg) = newImgHitsFaceBox(chosenPatchIdx(1,iImg),iImg);
+    end
+    
+    combMatrix_output(iDoublet,:) = [curDoubletInfo(1:4) nnz(newImgHitsFaceBox_Scaled)];
+    
+    
+    % clear variables
+    curDoubletInfo = [];
+    newC2_Scaled = [];
+    newImgHitsFaceBox = [];
+    chosenPatchIdx = [];
+end
+
+maxNewLocalization = max(combMatrix_output(:,5))/nImgs*100
+
+% save everything
+save(fullfile(saveLoc,'combMatrix_output'),'combMatrix_output');
+
+runDateTime = datetime('now');
+scriptName  = mfilename('fullpath');
+save(fullfile(saveLoc,'runParameters.mat'),...
+    'runDateTime',...
+    'scriptName',...
+    'runParameterComments',...
+    'saveLoc',...
+    'loadLoc_singles',...
+    'perfUsedTraining',...
+    'perfUsedTesting'...
+    );
+
+end
---
+++ humanae_processing/paste_Florence_Faces_in_Jacob_bgs.m
@@ -0,0 +1,144 @@
+% This is an adaptation of pasteHumanaeFaces.m
+% This script will use Florence's faces and Jacob's bgs to paste the faces
+% in the bgs. This was used to evaluate HMAX on such images and see if
+% performance would be comparable to Florence's faces pasted in Florence's
+% backgrounds. 
+
+clear; clc; dbstop if error; close all;
+
+home    = 'C:\Users\levan\HMAX\annulusExptFixedContrast\simulation4';
+faceLoc = 'C:\Users\levan\HMAX\annulusExptFixedContrast\face_images_from_Florence';
+condition = 'control_v3';
+
+
+bgPaths   = lsDir(fullfile(home,'control_v2','annulizedImages_730x927'),{'png'});
+bgPaths   = horzcat(bgPaths,bgPaths); % make it twice as large, so each bg repeats twice.
+facePaths = lsDir(fullfile(faceLoc,'used_in_psych_cropped'),{'png'});
+maskPaths = lsDir(fullfile(faceLoc,'MaskToKeep_cropped'),{'jpg'});
+
+nImgsGenerated = length(bgPaths);
+maxSizeFace    = 81;
+annulusRadius  = 284; % pixels
+
+% For each bg, choose a face randomly to paste it in it.
+faceForEachBg  = repmat(1:length(facePaths),1,20); % Generate a vector of indices for bgs.
+randomizeFaces = randperm(length(faceForEachBg)); % shuffle the vector.
+faceForEachBg  = faceForEachBg(randomizeFaces);     % shuffle the vector.
+faceForEachBg  = faceForEachBg(1:nImgsGenerated); % shorten the vector to the number of face imgs.
+
+% Define the circle along which to paste the face.
+exampleBg = imread(bgPaths{1});
+bgDims    = size(exampleBg);
+theta = 0 : 0.01: 2*pi;
+xCenter = size(exampleBg,2)/2;
+yCenter = size(exampleBg,1)/2;
+xValues = round(annulusRadius * cos(theta) + xCenter);
+yValues = round(yCenter - annulusRadius * sin(theta));
+
+% plot(xValues,yValues);
+imgCounter = 1;
+sanity = [];
+
+for iImg = 1:nImgsGenerated
+    iImg
+    [~,faceFileName,faceExt] = fileparts(facePaths{faceForEachBg(iImg)});
+    [~,bgFileName,bgExt]     = fileparts(bgPaths{iImg});
+    
+    % Find idx of the mask corresponding to the face.
+%     idx_mask = find(ismember(maskPaths,...
+%         fullfile(humanaeLoc,'HumanaeFaces5Processed','Mask',[faceFileName '.jpg'])));
+%     
+    % Load bg, face, and mask, and facepositions
+%     bg      = grayImage(imread(bgPaths{faceForEachBg(iImg)}))/255;
+%     bg      = imresize(bg,resizeBg/size(bg,1)); % rescale the bg so its not too large. Simulations will run faster.
+%     faceImg = grayImage(imread(facePaths{iImg}))/255;
+%     maskImg = grayImage(imread(maskPaths{idx_mask}));
+
+    bg      = grayImage(imread(bgPaths{iImg}))/255;
+    faceImg = grayImage(imread(facePaths{faceForEachBg(iImg)}))/255;
+    maskImg = grayImage(imread(fullfile(faceLoc,'MaskToKeep_cropped',[faceFileName '.png'])));
+    
+    faceImg = resizeImage(faceImg,maxSizeFace);
+    maskImg = resizeImage(maskImg,maxSizeFace);
+    assert(isequal(size(faceImg),size(maskImg)) == 1,'resized dimensions of face and mask don''t match');    
+   
+    % Choose location to paste the face in.
+    permLocs = randperm(length(xValues)); % generate random indices for choosing xValues and yValues for pasting the face.
+    for iLoc = 1:length(permLocs)
+        pasteLocation = [yValues(permLocs(iLoc)) xValues(permLocs(iLoc)) ];
+    
+        bgPatch = bg(pasteLocation(1) - ceil (size(faceImg,1)/2):  ...
+                     pasteLocation(1) + floor(size(faceImg,1)/2)-1,...
+                     pasteLocation(2) - ceil (size(faceImg,2)/2):  ...
+                     pasteLocation(2) + floor(size(faceImg,2)/2)-1);
+        
+        faceImg_norm = stretch_histogram(faceImg,bgPatch);
+        
+        face_michelson = (max(faceImg_norm(:)) - min(faceImg_norm(:)))/...
+                         (max(faceImg_norm(:)) + min(faceImg_norm(:)));
+        if face_michelson > 0.05
+            break
+        end
+    end
+    
+    % If some location gave a good contrast value paste the face. Otherwise
+    % go to the next background.
+    if face_michelson > 0.05
+    
+        % Get the polar angle of the location where face will be pasted.
+        cartX = pasteLocation(2) - xCenter;
+        cartY = yCenter - pasteLocation(1);
+        [THETA,RHO] = cart2pol(cartX,cartY);
+        angle_deg = rad2deg(THETA);
+        THETA(THETA < 0) = THETA(THETA < 0) + 2*pi; % just to have them all positive.
+        if angle_deg <= 0
+            angle_deg = 360 + angle_deg;
+        end
+        
+        % Sanity check
+        assert(isequal(size(faceImg),size(bgPatch)),'bgPatch and faceImg sizes don''t match');
+%         sanity(1:2,imgCounter) = [THETA; theta(permLocs(iLoc))];
+        assert(isequal(round(THETA,2),round(theta(permLocs(iLoc)),2)),'The angle location for pasting is messed up');
+        
+        %     imshow(faceImg);
+        %     figure
+        %     imshow(faceImg_norm);
+        
+        pastedBg = bg;
+        for iCol = 1:size(faceImg,2)
+            %         iCol
+            for iRow = 1:size(faceImg,1)
+                if maskImg(iRow,iCol) > 50
+                    % taking the ceil of the faceImg dimension might be a
+                    % problem, but should lead to an error of utmost 1 pixel
+                    % not being pasted in.
+                    pastedBg(pasteLocation(1) - ceil(size(faceImg,1)/2) + iRow,...
+                        pasteLocation(2) - ceil(size(faceImg,2)/2) + iCol) = ...
+                        faceImg_norm(iRow,iCol);
+                end
+            end
+        end
+%             figure
+%             imshow(pastedBg);
+        
+        % create exptDesign file
+        exptDesign(imgCounter).faceImg       = faceImg_norm;
+        exptDesign(imgCounter).faceName      = faceFileName;
+        exptDesign(imgCounter).bgName        = bgFileName;
+        exptDesign(imgCounter).position      = pasteLocation;
+        exptDesign(imgCounter).positionAngle = angle_deg;
+        exptDesign(imgCounter).mask          = maskImg;
+        exptDesign(imgCounter).imageDim      = bgDims;
+        exptDesign(imgCounter).michelson     = face_michelson;
+%         
+%             imwrite(pastedBg,fullfile('C:\Users\levan\HMAX\annulusExptFixedContrast\simulation4',condition,'images',...
+%                 ['img' int2str(imgCounter) '__' faceFileName '__' bgFileName '.png']));
+        
+        imgCounter = imgCounter + 1;
+    else % if contrast is good loop.
+        badBg{1} = bgPaths{iImg};
+    end
+end
+
+%% save necessary files
+save(fullfile(home,condition,'exptDesign.mat'),'exptDesign');
